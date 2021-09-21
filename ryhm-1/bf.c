c6eb7d2357585dbf5756e0bc58507b448ab89603
Kerdo Kurs <kerdo.kurs@ut.ee>
2021-09-21 15:38:05 +0300

    3. praktikum

--- ryhm-1/bf.c
+++ ryhm-1/bf.c
@@ -2,134 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MEM_LENGTH 8
-#define MEM_WIDTH 8
-#define MEM_HEIGHT (MEM_LENGTH / MEM_WIDTH)
-
-typedef struct
-{
-	char memory[MEM_LENGTH];
-	int pointer;
-	int sulge;
-} interp_muutujad_t;
-
-enum Error
-{
-	OK = 0,
-	OVERFLOW = 1,
-	UNDERFLOW = 2
-};
-
-interp_muutujad_t muutujad;
-
-// Prindib brainfucki mälu sisu
-void print_memory()
-{
-  int i = 0;
-  for (; i < MEM_HEIGHT; i++)
-  {
-    int j = 0;
-    for (; j < MEM_WIDTH; j++)
-    {
-      int idx = i * MEM_HEIGHT + j;
-      printf("%d ", muutujad.memory[idx]);
-    }
-
-    printf("\n");
-  }
-}
+#include "bf.h"
+#include "error.h"
+#include "mem_array.h"
 
 /*
 * Interpreteerib brainfucki.
 * src - karakterite massiiv, mille lõpus on null-byte
 */
-void interp(const char *src)
+void interp(const char *src, char* result)
 {
   int i = 0;
   char c;
   int code_length = strlen(src);
+
   while (c = src[i])
   {
     switch (c)
     {
     case '+':
-      muutujad.memory[muutujad.pointer]++;
+      inc();
       break;
     case '-':
-      muutujad.memory[muutujad.pointer]--;
+      dec();
       break;
     case '<':
-      if (muutujad.pointer <= 0) {
-	printf("Pointer underflow\n");
-	exit(UNDERFLOW);
-      }
-
-      muutujad.pointer--;
+      move_left();
       break;
     case '>':
-      if (muutujad.pointer >= MEM_LENGTH) {
-	printf("Pointer overflow");
-	exit(OVERFLOW);
-      }
-
-      muutujad.pointer++;
+      move_right();
       break;
     case '.':
-      printf("%c", muutujad.memory[muutujad.pointer]);
+      print(result);
       break;
     case ',':
-      scanf("%c", &muutujad.memory[muutujad.pointer]);
+      input();
       break;
     case '[':
-      if (muutujad.memory[muutujad.pointer] == 0)
-      {
-        muutujad.sulge = 1;
-
-	while (muutujad.sulge) {
-		if (i >= code_length) {
-			printf("Overflow with code pointer");
-			exit(OVERFLOW);
-		}
-
-		i++;
-                if (src[i] == '[') muutujad.sulge++;
-                if (src[i] == ']') muutujad.sulge--;
-	}
-      }
-
+      i = left_loop(i, src, code_length);
       break;
     case ']':
-      if (muutujad.memory[muutujad.pointer] != 0)
-      {
-        muutujad.sulge = -1;
-
-	while (muutujad.sulge) {
-		if (i <= 0) {
-			printf("Underflow with code pointer");
-			exit(UNDERFLOW);
-		}
-
-		i--;
-                if (src[i] == '[') muutujad.sulge++;
-                if (src[i] == ']') muutujad.sulge--;
-	}
-      }
-
+      i = right_loop(i, src, code_length);
+      break;
+    case '$':
+      // TODO: Implement yourself
+      // For example: print memory location or memory content as int
+      break;
+    case '#':
       break;
     }
 
     i++;
   }
-}
-
-int main()
-{
-  // hello world programm
-  // const char* str = "++++++++[>++++[>++>++>+++>+++>+<<<<-]>+>+>->>[<][<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++. ";
-  const char *str = "++[>+<-]";
-  interp(str);
-  print_memory();
-
-  // output peaks olema 0 2 0 ...
-  return 0;
 }
